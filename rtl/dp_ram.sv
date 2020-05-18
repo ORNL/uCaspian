8ae22337081095a9a2b247f6f4876947f900adbb
Parker Mitchell <jmitch47@utk.edu>
2020-05-17 20:36:41 -0400

    synapse -> registered dp ram + converting back to conservative synapse timings

--- rtl/dp_ram.sv
+++ rtl/dp_ram.sv
@@ -58,4 +58,29 @@ end
 
 endmodule
 
+module dp_ram_16x1024(
+    input               clk,
+    input               reset,
+
+    // Read Port
+    input         [9:0] rd_addr,
+    input               rd_en,
+    output logic [15:0] rd_data,
+
+    // Write Port
+    input         [9:0] wr_addr,
+    input               wr_en,
+    input  logic [15:0] wr_data
+);
+
+logic [15:0] ram [1023:0];
+
+always_ff @(posedge clk) begin
+    // Read before write
+    if(~reset & rd_en) rd_data <= ram[rd_addr];
+    if(~reset & wr_en) ram[wr_addr] <= wr_data;
+end
+
+endmodule
+
 `endif
--- rtl/synapse.sv
+++ rtl/synapse.sv
@@ -9,6 +9,8 @@
 `ifndef uCaspian_Synapse_SV
 `define uCaspian_Synapse_SV
 
+`include "dp_ram.sv"
+
 module ucaspian_synapse(
     input               clk,
     input               reset,
@@ -39,111 +41,128 @@ module ucaspian_synapse(
     input               dend_rdy
 );
 
-// states
-localparam [1:0]
-    SYN_IDLE                = 0,
-    SYN_ACTIVE              = 1,
-    SYN_BLOCK_ON_DENDRITE   = 2,
-    SYN_DISABLE             = 3;
 
-logic [1:0] synapse_state;
-initial synapse_state = SYN_IDLE;
 
-// 16x1k central configuration RAM
-//   On Lattice ice40, this will be four 4kbit BRAMs
-logic [15:0] synapse_cfg [1023:0];
+// Configuration RAM
+//   [15:8] synaptic weight - 8bits, signed
+//    [7:0] target neuron address 
+logic  [9:0] config_rd_addr;
+logic [15:0] config_rd_data;
+logic        config_rd_en;
+logic  [9:0] config_wr_addr;
+logic [15:0] config_wr_data;
+logic        config_wr_en;
+
+dp_ram_16x1024 config_ram_inst(
+    .clk(clk),
+    .reset(reset),
+
+    .rd_addr(config_rd_addr),
+    .rd_data(config_rd_data),
+    .rd_en(config_rd_en),
+
+    .wr_addr(config_wr_addr),
+    .wr_data(config_wr_data),
+    .wr_en(config_wr_en)
+);
 
 // configuration
-logic [7:0] cfg_weight;
 logic [9:0] clear_addr;
-logic clear_cfg_done;
+logic       clear_cfg_done;
 always_ff @(posedge clk) begin
-    if(clear_config) begin
-        if(!clear_cfg_done) begin
-            clear_addr <= clear_addr + 1;
-            synapse_cfg[clear_addr] <= 0;
+    config_wr_en <= 0;
 
-            if(clear_addr == 1023) begin
-                clear_cfg_done <= 1;
-            end
+    if(clear_config) begin
+        if(clear_addr == 1023) clear_cfg_done <= 1;
+
+        if(~clear_cfg_done) begin
+            clear_addr <= clear_addr + 1;
+            config_wr_addr <= clear_addr;
+            config_wr_data <= 0;
+            config_wr_en   <= 1;
         end
+    end
+    else if(cfg_enable) begin
+        config_wr_addr <= cfg_addr;
+        case(cfg_byte)
+            2: config_wr_data[15:8] <= cfg_value;
+            3: begin 
+                config_wr_data[7:0] <= cfg_value;
+                config_wr_en        <= 1;
+            end
+        endcase
     end
     else begin
-        clear_cfg_done  <= 0;
-        clear_addr      <= 0;
-
-        if(cfg_enable) begin
-            case(cfg_byte)
-                // weight
-                2: cfg_weight <= cfg_value;
-                // target neuron
-                3: synapse_cfg[cfg_addr] <= {cfg_weight, cfg_value};
-            endcase
-        end
+        clear_cfg_done <= 0;
+        clear_addr     <= 0;
     end
 end
 
 always_ff @(posedge clk) begin
-    if(clear_act)
-        clear_done <= 1;
-    else if(clear_config)
-        clear_done <= clear_cfg_done;
-    else
-        clear_done <= 0;
+    if(clear_act) clear_done <= 1;
+    else if(clear_config) clear_done <= clear_cfg_done;
+    else clear_done <= 0;
 end
 
-logic [9:0] syn_addr_reg;
+// Synapse Pipeline
+logic [9:0]  addr_dly;
+logic [15:0] data_dly;
+logic        rd_dly;
 
-logic [15:0] scfg_temp;
-logic [7:0]  dend_addr_temp;
-logic [7:0]  dend_ch_temp;
-always_comb begin
-    scfg_temp      = synapse_cfg[syn_addr_reg];
-    dend_ch_temp   = scfg_temp[15:8];
-    dend_addr_temp = scfg_temp[7:0];
-end
-
-always_comb begin
-    syn_rdy = dend_rdy || (~dend_vld && ~next_syn);
-end
-
-logic next_syn;
 always_ff @(posedge clk) begin
+    //// Step 1: Get incoming synapse
+    if(syn_rdy && syn_vld) begin
+        config_rd_addr <= syn_addr;
+        config_rd_en   <= 1;
+    end
+    else begin
+        config_rd_en   <= 0;
+    end
+
+    //// Step 2: Wait for data
+    addr_dly <= config_rd_addr;        
+    rd_dly   <= config_rd_en;
+
+    //// Step 3: Ouptut data
+    if(dend_rdy && dend_vld) begin
+        dend_vld <= 0;
+    end
+
+    if(rd_dly) begin
+        if(!(dend_vld && ~dend_rdy)) begin
+            dend_charge <= config_rd_data[15:8];
+            dend_addr   <= config_rd_data[7:0];
+            dend_vld    <= 1;
+        end
+        else begin
+            rd_dly <= 1; // todo
+        end
+    end
 
     if(reset || clear_act || clear_config) begin
-        syn_addr_reg <= 0;
-        dend_addr    <= 0;
-        dend_charge  <= 0;
-        dend_vld     <= 0;
-        next_syn     <= 0;
-    end
-    else if(enable) begin
-        if(dend_rdy && dend_vld) begin
-            dend_vld <= 0;
-            next_syn <= 0;
-        end
-
-        if(next_syn) begin
-            dend_addr   <= dend_addr_temp;
-            dend_charge <= dend_ch_temp;
-            dend_vld    <= 1;
-            next_syn    <= 0;
-        end
-        
-        if(syn_rdy && syn_vld) begin
-            syn_addr_reg <= syn_addr;
-            next_syn     <= 1;
-        end
-        else if(dend_vld && ~dend_rdy) begin
-            next_syn     <= 0;
-        end
+        dend_addr   <= 0;
+        dend_charge <= 0;
+        dend_vld    <= 0;
+        addr_dly    <= 0;
+        rd_dly      <= 0;
     end
 end
 
+always_comb begin
+    syn_rdy = ~dend_vld && ~rd_dly && ~config_rd_en;
+end
+
+/*
+always_comb begin
+    if(dend_rdy) syn_rdy = 1;
+    else if(~dend_vld && ~rd_dly && ~config_rd_en) syn_rdy = 1;
+    else syn_rdy = 0;
+end
+*/
+
 always_ff @(posedge clk) begin
-    step_done <= ~dend_vld && ~syn_vld && ~next_syn;
+    step_done <= ~dend_vld && ~syn_vld && ~config_rd_en && ~rd_dly;
 end
 
 endmodule
-
 `endif
